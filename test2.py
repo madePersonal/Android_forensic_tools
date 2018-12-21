e6705466d0644bb039235f29d0a206903e689568
sartika <saretik65@gmail.com>
2018-12-21 11:56:49 +0800

    2018-12-21, hashing

--- test2.py
+++ test2.py
@@ -1,5 +1,4 @@
 from pyand import ADB, Fastboot
-import pymysql
 import sqlite3
 
 adb = ADB()
@@ -15,9 +14,11 @@ already_dir = []
 
 # con = pymysql.connect(host='localhost', port=3306, user='root', passwd='', db='andr_forensic_tools')
 # cur = con.cursor()
-
-con = sqlite3.connect("andr_forensic_tools.db")
-cur = con.cursor()
+try:
+    con = sqlite3.connect("andr_forensic_tools.db")
+    cur = con.cursor()
+except Exception as e:
+    print(e.message)
 
 # line = file.split('\n')
 # y = (str(line[3]).split(' '))
@@ -35,6 +36,26 @@ cur = con.cursor()
 #         n = n + 1
 #     print(re[0])
 
+def clean_db():
+    try:
+        cur.execute("DELETE FROM sub_directory;")
+        con.commit()
+        cur.execute("DELETE FROM SQLITE_SEQUENCE WHERE name='sub_directory';")
+        con.commit()
+
+        cur.execute("DELETE FROM  file;")
+        con.commit()
+        cur.execute("DELETE FROM SQLITE_SEQUENCE WHERE name='file';")
+        con.commit()
+
+        cur.execute("DELETE FROM  directory;")
+        con.commit()
+        cur.execute("DELETE FROM SQLITE_SEQUENCE WHERE name='directory';")
+        con.commit()
+    except Exception as e:
+        print("clean DB error :"+e.args[0])
+
+
 
 
 def creat_array(text):
@@ -46,23 +67,22 @@ def creat_array(text):
         h = filter(None, y)
         if '->' in h:
             h.remove('->')
-        o.append(h)
 
+        o.append(h)
         n = n + 1
     del o[0]
     return o
 
 def insertToDB(dir):
-    id_dir=""
-    new_dir=None
-    new_dir =[]
+    id_dir=1
+
     cur.execute('INSERT INTO `directory` (`name`) VALUES ("%s")' % (dir))
     con.commit()
 
-    cur.execute('SELECT `id_directory` FROM directory WHERE name ="%s"'%(dir))
-    id= cur.fetchone()
-    for k in id :
-        id_dir =k
+    # cur.execute('SELECT `id_directory` FROM directory WHERE name ="%s"'%(dir))
+    # id= cur.fetchone()
+    # for k in id :
+    #     id_dir =k
 
     text = adb.shell_command('ls '+dir+' -l')
     array=creat_array(text)
@@ -86,12 +106,17 @@ def insertToDB(dir):
         dir_name = cur.fetchall()
         for u in dir_name:
             insertToDB2(dir+u[0])
-        # cur.execute('SELECT directory.`name`, sub_directory.name FROM sub_directory, `directory` WHERE sub_directory.id_directory=directory.id_directory AND directory.name = "%s"'%(dir+u[0]))
-        # new_dir_name = cur.fetchone()
-        # new_dir.append(new_dir_name[0]+new_dir_name[1])
-        #
-        # for h in new_dir :
-        #     insertToDB2(h[0])
+
+        while True :
+            id_dir = id_dir+1
+            try :
+                cur.execute('SELECT directory.`name`, sub_directory.name FROM sub_directory, `directory` WHERE sub_directory.id_directory and directory.id_directory=%s' %(id_dir))
+                dir_name = cur.fetchall()
+                for u in dir_name:
+                    insertToDB2(u[0]+u[1])
+            except Exception as e:
+                print(e.args[0])
+                break
 
 
 def insertToDB2(dir):
@@ -126,7 +151,14 @@ def insertToDB2(dir):
 
 insertToDB("/")
 
+#clean_db()
 
+# cur.execute('SELECT directory.`name`, sub_directory.name FROM sub_directory, `directory` WHERE sub_directory.id_directory=directory.id_directory AND directory.name = "%s"'%(dir+u[0]))
+        # new_dir_name = cur.fetchone()
+        # new_dir.append(new_dir_name[0]+new_dir_name[1])
+        #
+        # for h in new_dir :
+        #     insertToDB2(h[0])
 
 
 
--- test3.py
+++ test3.py
@@ -1,34 +1,77 @@
-import pymysql
-from pyand import ADB
-
-adb = ADB()
-adb = ADB()
-dev = adb.get_devices()
-adb.set_target_by_id(0)
-adb.get_target_device()
-serial= adb.get_serialno()
-
-# con = pymysql.connect(host='localhost', port=3306, user='root', passwd='', db='andr_forensic_tools')
-# cur = con.cursor()
+# import pymysql
+# from pyand import ADB
 #
-# cur.execute('SELECT directory.`name`, sub_directory.name FROM sub_directory, `directory` WHERE sub_directory.id_directory=directory.id_directory AND directory.name = "/cache/"')
-# new_dir_name = cur.fetchone()
+# adb = ADB()
+# adb = ADB()
+# dev = adb.get_devices()
+# adb.set_target_by_id(0)
+# adb.get_target_device()
+# serial= adb.get_serialno()
 #
-# print(new_dir_name)
-def creat_array(text):
-    n= 0
-    o = []
-    line = text.split("\n")
-    for l in line:
-        y = (str(line[n]).split(" "))
-        h = filter(None, y)
-        if '->' in h:
-            h.remove('->')
-        o.append(h)
-        n = n + 1
-    del o[0]
-    return o
+# # con = pymysql.connect(host='localhost', port=3306, user='root', passwd='', db='andr_forensic_tools')
+# # cur = con.cursor()
+# #
+# # cur.execute('SELECT directory.`name`, sub_directory.name FROM sub_directory, `directory` WHERE sub_directory.id_directory=directory.id_directory AND directory.name = "/cache/"')
+# # new_dir_name = cur.fetchone()
+# #
+# # print(new_dir_name)
+# def creat_array(text):
+#     n= 0
+#     o = []
+#     line = text.split("\n")
+#     for l in line:
+#         y = (str(line[n]).split(" "))
+#         h = filter(None, y)
+#         if '->' in h:
+#             h.remove('->')
+#         o.append(h)
+#         n = n + 1
+#     del o[0]
+#     return o
+#
+# text = adb.shell_command('ls / -l')
+#
+# print(creat_array(text))
+import sqlite3
+import hashlib
 
-text = adb.shell_command('ls / -l')
+try:
+    con = sqlite3.connect("andr_forensic_tools.db")
+    cur = con.cursor()
+except Exception as e:
+    print(e.message)
 
-print(creat_array(text))
+
+
+import time
+
+
+def test():
+    d = 1
+    while True:
+        d = d+1
+        cur.execute('SELECT directory.`name`, sub_directory.name FROM sub_directory, `directory` WHERE sub_directory.id_directory and directory.id_directory=%s'%d)
+        dir_name = cur.fetchall()
+        for u in dir_name:
+            print(u[0] + u[1])
+        time.sleep(1)
+
+def md5(file):
+    hasher = hashlib.md5()
+    with open(file, 'rb') as afile:
+        buf = afile.read()
+        hasher.update(buf)
+    return hasher.hexdigest()
+
+def sha1(file):
+    hasher = hashlib.sha1()
+    with open(file, 'rb') as afile:
+        buf = afile.read()
+        hasher.update(buf)
+    return hasher.hexdigest()
+
+print (md5("videoplayback.mp4"))
+print (md5("file/videoplayback.mp4"))
+
+print(sha1("566-1191-1-PB.pdf"))
+print(sha1("file/566-1191-1-PB.pdf"))
